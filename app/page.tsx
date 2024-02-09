3260bfd00887bf72228d5b1467fe821fc170d996
dmitridrozdov <dm.drozdov@gmail.com>
2024-02-09 13:07:01 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <p>hello there</p>
+    <Button>button</Button>
   );
 }
