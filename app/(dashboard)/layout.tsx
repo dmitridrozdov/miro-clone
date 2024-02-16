2098828947193751598b4b42238845771330cd1a
dmitridrozdov <dm.drozdov@gmail.com>
2024-02-17 10:23:28 +1100

    Update layout.tsx

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -11,18 +11,16 @@ const DashboardLayout = ({
 }: DashboardLayoutProps) => {
   return (
     <main className="h-full">
-        <Sidebar />
-        {children}
-       
-      {/*<div className="pl-[60px] h-full">
+      <Sidebar />
+      <div className="pl-[60px] h-full">
         <div className="flex gap-x-3 h-full">
-          <OrgSidebar />
+          {/* <OrgSidebar /> */}
           <div className="h-full flex-1">
-            <Navbar />
+            {/* <Navbar /> */}
             {children}
           </div>
         </div>
-      </div> */}
+      </div>
     </main>
   );
 };
