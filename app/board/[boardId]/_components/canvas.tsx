ecebab00fa1b5881caaaf476f12832aa82ae1be4
dmitridrozdov <dm.drozdov@gmail.com>
2024-05-09 15:49:22 +1000

    Update canvas.tsx

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,9 +1,10 @@
 "use client";
+import Info from './info'
 
 const Canvas = () => {
   return (
-    <main className="h-full w-full relative bg-neutral-100 touch-none">
-
+    <main className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex item-center shadow-md">
+        <Info />
     </main>
   )
 }
