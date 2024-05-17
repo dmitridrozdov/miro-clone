ab8717726e75f35a357828edc4157aba9c1fdbc2
dmitridrozdov <dm.drozdov@gmail.com>
2024-05-17 14:28:12 +1000

    Update info.tsx

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,6 +1,8 @@
 const Info = () => {
   return (
-    <div>info</div>
+    <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex item-center shadow-md">
+        TODO: information about the board
+    </div>
   )
 }
 
